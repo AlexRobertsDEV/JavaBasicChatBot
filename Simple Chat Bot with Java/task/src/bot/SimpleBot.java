14d6a42a5060e99888b9bb1ec35ec283cd5578e4
AlexRoberts <128462363+AlexRobertsDEV@users.noreply.github.com>
2023-12-13 11:51:50 -0500

    Update SimpleBot.java

--- Simple Chat Bot with Java/task/src/bot/SimpleBot.java
+++ Simple Chat Bot with Java/task/src/bot/SimpleBot.java
@@ -3,7 +3,7 @@ package bot;
 import java.util.Scanner;
 
 public class SimpleBot {
-    final static Scanner scanner = new Scanner(System.in); // Do not change this line
+    final static Scanner scanner = new Scanner(System.in); 
 
     public static void main(String[] args) {
         chatBotGreeting("Nova", 2023);
@@ -48,7 +48,7 @@ public class SimpleBot {
         System.out.println("Let's test your programming knowledge.");
         boolean correctAnswer = false;
 
-        while (!correctAnswer) { // Loop should continue while the correct answer has not been given
+        while (!correctAnswer) { 
             System.out.println("Why do we use methods?");
             System.out.println("1. To repeat a statement multiple times.");
             System.out.println("2. To decompose a program into several small subroutines.");
@@ -66,6 +66,6 @@ public class SimpleBot {
     }
 
     static void end() {
-        System.out.println("Congratulations, have a nice day!"); // Do not change this text
+        System.out.println("Congratulations, have a nice day!"); 
     }
 }
